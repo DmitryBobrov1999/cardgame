01596d34d36866f2fc79fc1d79a38f5269ccd4a9
Travel1999 <bobrovdmitry01@mail.ru>
2023-07-06 12:07:11 +0300

    hw_3_1

--- src/index.js
+++ src/index.js
@@ -10,6 +10,9 @@ let chooseInput1 = document.getElementById('choose_input_1')
 let chooseInput2 = document.getElementById('choose_input_2')
 let chooseInput3 = document.getElementById('choose_input_3')
 
+import { hiddenCardsHTML } from '/src/hiddenCards.js'
+import { cardsHTML } from '/src/cards.js'
+
 for (const radio of radios) {
     radio.onchange = getChooseDif
 }
@@ -35,6 +38,7 @@ function getChooseDif() {
     }
 }
 
+
 form.addEventListener('submit', (event) => {
     event.preventDefault()
     if (chooseInput1.checked) {
@@ -47,111 +51,3 @@ form.addEventListener('submit', (event) => {
         BODY.innerHTML = 'Страница сложности 3'
     }
 })
-
-const hiddenCardsHTML = `<div class='pageCardsDiv'>
-<div class="timerAndButton">
-<div class="timer"><div class="minAndSek"><p class="min">min</p>
-<p class="sek">sek</p>
-</div><p class="theMainTimer">00.00</p></div>
-<button class="buttonAgain">Начать заново</button>
-</div><div class="allCards">
-<div class="cardsString">
-<div class="cardsSolo"><img class="cardsImg" src ="/img/рубашка.svg"></div>
-<div class="cardsSolo"><img class="cardsImg" src ="/img/рубашка.svg"></div>
-<div class="cardsSolo"><img class="cardsImg" src ="/img/рубашка.svg"></div>
-<div class="cardsSolo"><img class="cardsImg" src ="/img/рубашка.svg"></div>
-<div class="cardsSolo"><img class="cardsImg" src ="/img/рубашка.svg"></div>
-<div class="cardsSolo"><img class="cardsImg" src ="/img/рубашка.svg"></div>
-<div class="cardsSolo"><img class="cardsImg" src ="/img/рубашка.svg"></div>
-<div class="cardsSolo"><img class="cardsImg" src ="/img/рубашка.svg"></div>
-<div class="cardsSolo"><img class="cardsImg" src ="/img/рубашка.svg"></div>
-</div>
-<div class="cardsString">
-<div class="cardsSolo"><img class="cardsImg" src ="/img/рубашка.svg"></div>
-<div class="cardsSolo"><img class="cardsImg" src ="/img/рубашка.svg"></div>
-<div class="cardsSolo"><img class="cardsImg" src ="/img/рубашка.svg"></div>
-<div class="cardsSolo"><img class="cardsImg" src ="/img/рубашка.svg"></div>
-<div class="cardsSolo"><img class="cardsImg" src ="/img/рубашка.svg"></div>
-<div class="cardsSolo"><img class="cardsImg" src ="/img/рубашка.svg"></div>
-<div class="cardsSolo"><img class="cardsImg" src ="/img/рубашка.svg"></div>
-<div class="cardsSolo"><img class="cardsImg" src ="/img/рубашка.svg"></div>
-<div class="cardsSolo"><img class="cardsImg" src ="/img/рубашка.svg"></div>
-</div>
-<div class="cardsString">
-<div class="cardsSolo"><img class="cardsImg" src ="/img/рубашка.svg"></div>
-<div class="cardsSolo"><img class="cardsImg" src ="/img/рубашка.svg"></div>
-<div class="cardsSolo"><img class="cardsImg" src ="/img/рубашка.svg"></div>
-<div class="cardsSolo"><img class="cardsImg" src ="/img/рубашка.svg"></div>
-<div class="cardsSolo"><img class="cardsImg" src ="/img/рубашка.svg"></div>
-<div class="cardsSolo"><img class="cardsImg" src ="/img/рубашка.svg"></div>
-<div class="cardsSolo"><img class="cardsImg" src ="/img/рубашка.svg"></div>
-<div class="cardsSolo"><img class="cardsImg" src ="/img/рубашка.svg"></div>
-<div class="cardsSolo"><img class="cardsImg" src ="/img/рубашка.svg"></div>
-</div>
-<div class="cardsString">
-<div class="cardsSolo"><img class="cardsImg" src ="/img/рубашка.svg"></div>
-<div class="cardsSolo"><img class="cardsImg" src ="/img/рубашка.svg"></div>
-<div class="cardsSolo"><img class="cardsImg" src ="/img/рубашка.svg"></div>
-<div class="cardsSolo"><img class="cardsImg" src ="/img/рубашка.svg"></div>
-<div class="cardsSolo"><img class="cardsImg" src ="/img/рубашка.svg"></div>
-<div class="cardsSolo"><img class="cardsImg" src ="/img/рубашка.svg"></div>
-<div class="cardsSolo"><img class="cardsImg" src ="/img/рубашка.svg"></div>
-<div class="cardsSolo"><img class="cardsImg" src ="/img/рубашка.svg"></div>
-<div class="cardsSolo"><img class="cardsImg" src ="/img/рубашка.svg"></div>
-</div>
-</div>
-</div>`
-
-const cardsHTML = `<div class='pageCardsDiv'>
-<div class="timerAndButton">
-<div class="timer"><div class="minAndSek"><p class="min">min</p>
-<p class="sek">sek</p>
-</div><p class="theMainTimer">00.00</p></div>
-<button class="buttonAgain">Начать заново</button>
-</div><div class="allCards">
-<div class="cardsString">
-<div class="cardsSolo"><img class="cardsImg" src ="/img/туз пики.svg"></div>
-<div class="cardsSolo"><img class="cardsImg" src ="/img/король пики.svg"></div>
-<div class="cardsSolo"><img class="cardsImg" src ="/img/дама пики.svg"></div>
-<div class="cardsSolo"><img class="cardsImg" src ="/img/валет пики.svg"></div>
-<div class="cardsSolo"><img class="cardsImg" src ="/img/10 пики.svg"></div>
-<div class="cardsSolo"><img class="cardsImg" src ="/img/9 пики.svg"></div>
-<div class="cardsSolo"><img class="cardsImg" src ="/img/8 пики.svg"></div>
-<div class="cardsSolo"><img class="cardsImg" src ="/img/7 пики.svg"></div>
-<div class="cardsSolo"><img class="cardsImg" src ="/img/6 пики.svg"></div>
-</div>
-<div class="cardsString">
-<div class="cardsSolo"><img class="cardsImg" src ="/img/туз черви.svg"></div>
-<div class="cardsSolo"><img class="cardsImg" src ="/img/король черви.svg"></div>
-<div class="cardsSolo"><img class="cardsImg" src ="/img/дама черви.svg"></div>
-<div class="cardsSolo"><img class="cardsImg" src ="/img/валет черви.svg"></div>
-<div class="cardsSolo"><img class="cardsImg" src ="/img/10 черви.svg"></div>
-<div class="cardsSolo"><img class="cardsImg" src ="/img/9 черви.svg"></div>
-<div class="cardsSolo"><img class="cardsImg" src ="/img/8 черви.svg"></div>
-<div class="cardsSolo"><img class="cardsImg" src ="/img/7 черви.svg"></div>
-<div class="cardsSolo"><img class="cardsImg" src ="/img/6 черви.svg"></div>
-</div>
-<div class="cardsString">
-<div class="cardsSolo"><img class="cardsImg" src ="/img/туз бубны.svg"></div>
-<div class="cardsSolo"><img class="cardsImg" src ="/img/король бубны.svg"></div>
-<div class="cardsSolo"><img class="cardsImg" src ="/img/дама бубны.svg"></div>
-<div class="cardsSolo"><img class="cardsImg" src ="/img/валет бубны.svg"></div>
-<div class="cardsSolo"><img class="cardsImg" src ="/img/10 бубны.svg"></div>
-<div class="cardsSolo"><img class="cardsImg" src ="/img/9 бубны.svg"></div>
-<div class="cardsSolo"><img class="cardsImg" src ="/img/8 бубны.svg"></div>
-<div class="cardsSolo"><img class="cardsImg" src ="/img/7 бубны.svg"></div>
-<div class="cardsSolo"><img class="cardsImg" src ="/img/6 бубны.svg"></div>
-</div>
-<div class="cardsString">
-<div class="cardsSolo"><img class="cardsImg" src ="/img/туз крести.svg"></div>
-<div class="cardsSolo"><img class="cardsImg" src ="/img/король крести.svg"></div>
-<div class="cardsSolo"><img class="cardsImg" src ="/img/дама крести.svg"></div>
-<div class="cardsSolo"><img class="cardsImg" src ="/img/валет крести.svg"></div>
-<div class="cardsSolo"><img class="cardsImg" src ="/img/10 крести.svg"></div>
-<div class="cardsSolo"><img class="cardsImg" src ="/img/9 крести.svg"></div>
-<div class="cardsSolo"><img class="cardsImg" src ="/img/8 крести.svg"></div>
-<div class="cardsSolo"><img class="cardsImg" src ="/img/7 крести.svg"></div>
-<div class="cardsSolo"><img class="cardsImg" src ="/img/6 крести.svg"></div>
-</div>
-</div>
-</div>`
