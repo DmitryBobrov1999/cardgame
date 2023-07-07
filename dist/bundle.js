78b616a1dc44b7fedbad98a6991054dc86abd375
Travel1999 <bobrovdmitry01@mail.ru>
2023-07-07 16:32:51 +0300

    hw_4

---
+++ dist/bundle.js
@@ -0,0 +1,263 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/cards.js":
+/*!**********************!*\
+  !*** ./src/cards.js ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   cardsHTML: () => (/* binding */ cardsHTML)
+/* harmony export */ });
+const cardsHTML = `<div class='pageCardsDiv'>
+<div class="timerAndButton">
+<div class="timer"><div class="minAndSek"><p class="min">min</p>
+<p class="sek">sek</p>
+</div><p class="theMainTimer">00.00</p></div>
+<button class="buttonAgain">Начать заново</button>
+</div><div class="allCards">
+<div class="cardsString">
+<div class="cardsSolo"><img class="cardsImg" src ="/img/туз пики.svg"></div>
+<div class="cardsSolo"><img class="cardsImg" src ="/img/король пики.svg"></div>
+<div class="cardsSolo"><img class="cardsImg" src ="/img/дама пики.svg"></div>
+<div class="cardsSolo"><img class="cardsImg" src ="/img/валет пики.svg"></div>
+<div class="cardsSolo"><img class="cardsImg" src ="/img/10 пики.svg"></div>
+<div class="cardsSolo"><img class="cardsImg" src ="/img/9 пики.svg"></div>
+<div class="cardsSolo"><img class="cardsImg" src ="/img/8 пики.svg"></div>
+<div class="cardsSolo"><img class="cardsImg" src ="/img/7 пики.svg"></div>
+<div class="cardsSolo"><img class="cardsImg" src ="/img/6 пики.svg"></div>
+</div>
+<div class="cardsString">
+<div class="cardsSolo"><img class="cardsImg" src ="/img/туз черви.svg"></div>
+<div class="cardsSolo"><img class="cardsImg" src ="/img/король черви.svg"></div>
+<div class="cardsSolo"><img class="cardsImg" src ="/img/дама черви.svg"></div>
+<div class="cardsSolo"><img class="cardsImg" src ="/img/валет черви.svg"></div>
+<div class="cardsSolo"><img class="cardsImg" src ="/img/10 черви.svg"></div>
+<div class="cardsSolo"><img class="cardsImg" src ="/img/9 черви.svg"></div>
+<div class="cardsSolo"><img class="cardsImg" src ="/img/8 черви.svg"></div>
+<div class="cardsSolo"><img class="cardsImg" src ="/img/7 черви.svg"></div>
+<div class="cardsSolo"><img class="cardsImg" src ="/img/6 черви.svg"></div>
+</div>
+<div class="cardsString">
+<div class="cardsSolo"><img class="cardsImg" src ="/img/туз бубны.svg"></div>
+<div class="cardsSolo"><img class="cardsImg" src ="/img/король бубны.svg"></div>
+<div class="cardsSolo"><img class="cardsImg" src ="/img/дама бубны.svg"></div>
+<div class="cardsSolo"><img class="cardsImg" src ="/img/валет бубны.svg"></div>
+<div class="cardsSolo"><img class="cardsImg" src ="/img/10 бубны.svg"></div>
+<div class="cardsSolo"><img class="cardsImg" src ="/img/9 бубны.svg"></div>
+<div class="cardsSolo"><img class="cardsImg" src ="/img/8 бубны.svg"></div>
+<div class="cardsSolo"><img class="cardsImg" src ="/img/7 бубны.svg"></div>
+<div class="cardsSolo"><img class="cardsImg" src ="/img/6 бубны.svg"></div>
+</div>
+<div class="cardsString">
+<div class="cardsSolo"><img class="cardsImg" src ="/img/туз крести.svg"></div>
+<div class="cardsSolo"><img class="cardsImg" src ="/img/король крести.svg"></div>
+<div class="cardsSolo"><img class="cardsImg" src ="/img/дама крести.svg"></div>
+<div class="cardsSolo"><img class="cardsImg" src ="/img/валет крести.svg"></div>
+<div class="cardsSolo"><img class="cardsImg" src ="/img/10 крести.svg"></div>
+<div class="cardsSolo"><img class="cardsImg" src ="/img/9 крести.svg"></div>
+<div class="cardsSolo"><img class="cardsImg" src ="/img/8 крести.svg"></div>
+<div class="cardsSolo"><img class="cardsImg" src ="/img/7 крести.svg"></div>
+<div class="cardsSolo"><img class="cardsImg" src ="/img/6 крести.svg"></div>
+</div>
+</div>
+</div>`
+
+
+/***/ }),
+
+/***/ "./src/hiddenCards.js":
+/*!****************************!*\
+  !*** ./src/hiddenCards.js ***!
+  \****************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   hiddenCardsHTML: () => (/* binding */ hiddenCardsHTML)
+/* harmony export */ });
+const hiddenCardsHTML = `<div class='pageCardsDiv'>
+<div class="timerAndButton">
+<div class="timer"><div class="minAndSek"><p class="min">min</p>
+<p class="sek">sek</p>
+</div><p class="theMainTimer">00.00</p></div>
+<button class="buttonAgain">Начать заново</button>
+</div><div class="allCards">
+<div class="cardsString">
+<div class="cardsSolo"><img class="cardsImg" src ="/img/рубашка.svg"></div>
+<div class="cardsSolo"><img class="cardsImg" src ="/img/рубашка.svg"></div>
+<div class="cardsSolo"><img class="cardsImg" src ="/img/рубашка.svg"></div>
+<div class="cardsSolo"><img class="cardsImg" src ="/img/рубашка.svg"></div>
+<div class="cardsSolo"><img class="cardsImg" src ="/img/рубашка.svg"></div>
+<div class="cardsSolo"><img class="cardsImg" src ="/img/рубашка.svg"></div>
+<div class="cardsSolo"><img class="cardsImg" src ="/img/рубашка.svg"></div>
+<div class="cardsSolo"><img class="cardsImg" src ="/img/рубашка.svg"></div>
+<div class="cardsSolo"><img class="cardsImg" src ="/img/рубашка.svg"></div>
+</div>
+<div class="cardsString">
+<div class="cardsSolo"><img class="cardsImg" src ="/img/рубашка.svg"></div>
+<div class="cardsSolo"><img class="cardsImg" src ="/img/рубашка.svg"></div>
+<div class="cardsSolo"><img class="cardsImg" src ="/img/рубашка.svg"></div>
+<div class="cardsSolo"><img class="cardsImg" src ="/img/рубашка.svg"></div>
+<div class="cardsSolo"><img class="cardsImg" src ="/img/рубашка.svg"></div>
+<div class="cardsSolo"><img class="cardsImg" src ="/img/рубашка.svg"></div>
+<div class="cardsSolo"><img class="cardsImg" src ="/img/рубашка.svg"></div>
+<div class="cardsSolo"><img class="cardsImg" src ="/img/рубашка.svg"></div>
+<div class="cardsSolo"><img class="cardsImg" src ="/img/рубашка.svg"></div>
+</div>
+<div class="cardsString">
+<div class="cardsSolo"><img class="cardsImg" src ="/img/рубашка.svg"></div>
+<div class="cardsSolo"><img class="cardsImg" src ="/img/рубашка.svg"></div>
+<div class="cardsSolo"><img class="cardsImg" src ="/img/рубашка.svg"></div>
+<div class="cardsSolo"><img class="cardsImg" src ="/img/рубашка.svg"></div>
+<div class="cardsSolo"><img class="cardsImg" src ="/img/рубашка.svg"></div>
+<div class="cardsSolo"><img class="cardsImg" src ="/img/рубашка.svg"></div>
+<div class="cardsSolo"><img class="cardsImg" src ="/img/рубашка.svg"></div>
+<div class="cardsSolo"><img class="cardsImg" src ="/img/рубашка.svg"></div>
+<div class="cardsSolo"><img class="cardsImg" src ="/img/рубашка.svg"></div>
+</div>
+<div class="cardsString">
+<div class="cardsSolo"><img class="cardsImg" src ="/img/рубашка.svg"></div>
+<div class="cardsSolo"><img class="cardsImg" src ="/img/рубашка.svg"></div>
+<div class="cardsSolo"><img class="cardsImg" src ="/img/рубашка.svg"></div>
+<div class="cardsSolo"><img class="cardsImg" src ="/img/рубашка.svg"></div>
+<div class="cardsSolo"><img class="cardsImg" src ="/img/рубашка.svg"></div>
+<div class="cardsSolo"><img class="cardsImg" src ="/img/рубашка.svg"></div>
+<div class="cardsSolo"><img class="cardsImg" src ="/img/рубашка.svg"></div>
+<div class="cardsSolo"><img class="cardsImg" src ="/img/рубашка.svg"></div>
+<div class="cardsSolo"><img class="cardsImg" src ="/img/рубашка.svg"></div>
+</div>
+</div>
+</div>`
+
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+(() => {
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _hiddenCards_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./hiddenCards.js */ "./src/hiddenCards.js");
+/* harmony import */ var _cards_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./cards.js */ "./src/cards.js");
+let radios = document.getElementsByName('choose_input')
+let difText = document.querySelector('.chooseDifText')
+let label1 = document.querySelector('.label_1')
+let label2 = document.querySelector('.label_2')
+let label3 = document.querySelector('.label_3')
+let form = document.querySelector('.lightBlueSquare')
+let BODY = document.querySelector('.body')
+
+let chooseInput1 = document.getElementById('choose_input_1')
+let chooseInput2 = document.getElementById('choose_input_2')
+let chooseInput3 = document.getElementById('choose_input_3')
+
+;
+
+
+for (const radio of radios) {
+    radio.onchange = getChooseDif
+}
+
+function getChooseDif() {
+    if (this.value == 1) {
+        difText.innerHTML = 'Легкий уровень'
+        label1.style.background = '#7CFC00'
+        label2.style.background = '#ffffff'
+        label3.style.background = '#ffffff'
+    }
+    if (this.value == 2) {
+        difText.innerHTML = 'Средний уровень'
+        label2.style.background = '#FFFF00'
+        label1.style.background = '#ffffff'
+        label3.style.background = '#ffffff'
+    }
+    if (this.value == 3) {
+        difText.innerHTML = 'Сложный уровень'
+        label3.style.background = '#FF0000'
+        label1.style.background = '#ffffff'
+        label2.style.background = '#ffffff'
+    }
+}
+
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault()
+    if (chooseInput1.checked) {
+        BODY.innerHTML = _hiddenCards_js__WEBPACK_IMPORTED_MODULE_0__.hiddenCardsHTML
+    }
+    if (chooseInput2.checked) {
+        BODY.innerHTML = _cards_js__WEBPACK_IMPORTED_MODULE_1__.cardsHTML
+    }
+    if (chooseInput3.checked) {
+        BODY.innerHTML = 'Страница сложности 3'
+    }
+})
+
+
+})();
+
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
